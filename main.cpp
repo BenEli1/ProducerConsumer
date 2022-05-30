3a095efa513351c5fb329ee37db404b2b7824409
Ben Eli <benelashvili@gmail.com>
2022-05-30 15:49:43 +0300

    trying to use mutex and semaphores

--- main.cpp
+++ main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <string.h>
+#include <queue>
+#include <pthread.h>
+#include<thread>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <mutex>
 
+using namespace std;
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+class BoundedQueue:public queue<string>{
+    mutex m;	/* mutual exclusion semaphore  */
+    semaphore empty = N;	/* count of empty buffer slots */
+    semaphore full  = 0;	/* count of full  buffer slots */
+
+
+public:
+    BoundedQueue(int n) {
+        empty=n;
+        full=0;
+    }
+    void up(int lock){
+        lock++;
+    }
+    void down(int lock){
+        lock--;
+    }
+    void push(string x){
+        lock(m);
+    }
+    string pop(){
+
+    }
+
+};
